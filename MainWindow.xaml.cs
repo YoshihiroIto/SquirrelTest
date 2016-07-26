c70388b35deda0e055cf1b8d0f7ac602d65dbb44
Yoshihiro Ito <yo.i.jewelry.bab@gmail.com>
2016-07-26 23:50:00 +0900

    Add app

---
+++ MainWindow.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System.Reflection;
+
+namespace SquirrelTest
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            var assembly = Assembly.GetExecutingAssembly();
+
+            //Exeの場所を表示
+            ExeTextBox.Text = assembly.Location;
+
+            //バージョン番号を表示
+            VersionTextBox.Text = assembly.GetName().Version.ToString();
+        }
+    }
+}
